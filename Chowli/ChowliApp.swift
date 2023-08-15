68519cdd7cd9349e23c545e81d9f99c8f580cf0e
afreddo <freddoa@live.com>
2023-08-15 14:05:58 -0500

    Initial Commit

---
+++ Chowli/ChowliApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChowliApp.swift
+//  Chowli
+//
+//  Created by Alex Freddo on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ChowliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
